e0cda95d690553cbad9b8cebef6182481106b37b
William Mantegna <wmantegna@gmail.com>
2014-02-08 19:02:02 -0500

    changed mail server settings for SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,16 @@ Fermently::Application.configure do
 
   config.action_mailer.default_url_options = {:host => 'fermently.heroku.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => 25,
-    :domain  => 'fermently.heroku.com'
-  }
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
   # Settings specified here will take precedence over those in config/application.rb.
 
